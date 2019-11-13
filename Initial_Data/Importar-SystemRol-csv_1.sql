504ac70a9c510b6bcefdf1b185d231411e95112a
ErvinLGUMG <elaferreg@miumg.edu.gt>
2019-11-13 00:21:19 -0600

    Initial data scripts

---
+++ Initial_Data/Importar-SystemRol-csv_1.sql
@@ -0,0 +1,6 @@
+SET DEFINE OFF
+
+INSERT INTO SYSTEM_ROLES (ROLE_ID, NAME, STATE, CREATED_ON, CREATED_USER, MODIFIED_ON, MODIFIED_USER) 
+VALUES (1.0, 'Administrador', 1.0, to_timestamp('30/08/2019 02:51', 'YYYY-MM-DD HH24:MI:SS'), 'MADX', to_timestamp('30/08/2019 02:51', 'YYYY-MM-DD HH24:MI:SS'), 'NULL');
+
+-- Importar Datos a la Tabla SYSTEM_ROLES del Archivo C:\Users\Ervin LG\UMG20192\DesarrolloWeb\Data_proyect\SystemRol.csv . Tarea correcta enviada a hoja de trabajo.
